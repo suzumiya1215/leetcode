70542b04fbaf51cc69b2733e3f6f2ea133cf01eb
suzumiya1215 <1030011936@qq.com>
2017-08-27 17:35:58 +0800

    561. Array Partition I

---
+++ 561. Array Partition I.cpp
@@ -0,0 +1,13 @@
+﻿class Solution {
+public:
+    
+	int arrayPairSum1(vector<int>& nums)
+	{
+		sort(nums.begin(), nums.end());
+		int sum = 0;
+		for (vector<int>::iterator iter3 = nums.begin(); iter3 != nums.end(); ++++iter3)
+			sum += *iter3;
+
+		return sum;
+	}
+};
