a9e7d66d8603449989c9172adfd85ddca78543a6
suzumiya1215 <1030011936@qq.com>
2017-08-28 22:48:49 +0800

    344. Reverse String

---
+++ 344. Reverse String.cpp
@@ -0,0 +1,10 @@
+﻿class Solution {
+public:
+    string reverseString(string s)
+	{
+		string str;
+		for (string::reverse_iterator riter = s.rbegin(); riter != s.rend(); ++riter)
+			str.push_back(*riter);
+		return str;
+	}
+};
