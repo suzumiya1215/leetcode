61517c087a81f118f9b8ee3c6f88b94c5f284246
suzumiya1215 <1030011936@qq.com>
2017-08-28 22:51:00 +0800

    476. Number Complement

---
+++ 476. Number Complement.cpp
@@ -0,0 +1,20 @@
+﻿class Solution {
+public:
+    int findComplement(int num)
+	{
+		vector<int> vec;
+		int result = 0;
+		while (num != 1)
+		{
+			if (num % 2 == 0)
+				vec.push_back(1);
+			else if (num % 2 == 1)
+				vec.push_back(0);
+			num /= 2;
+		}
+		for (int i = 0; i != vec.size(); ++i)
+			result += vec[i] * pow(2, i);
+
+		return result;
+	}
+};
