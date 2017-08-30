6e3a52b72b1329076b53d914aeaa26e6cca6c6ea
suzumiya1215 <1030011936@qq.com>
2017-08-30 16:34:04 +0800

    575. Distribute Candies

---
+++ 575. Distribute Candies.cpp
@@ -0,0 +1,10 @@
+﻿class Solution {
+public:
+   //https://leetcode.com/problems/distribute-candies/description/
+	int distributeCandies(vector<int>& candies)
+	{
+		set<int> se;
+		se.insert(candies.begin(), candies.end());
+		return se.size() >= candies.size() / 2 ? candies.size() / 2 : se.size();
+	}
+};
