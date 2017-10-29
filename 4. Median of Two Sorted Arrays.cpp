4b806502aee227feb413f32f1d8ec168e8d70f1a
suzumiya1215 <1030011936@qq.com>
2017-10-29 11:51:51 +0800

    4. Median of Two Sorted Arrays

---
+++ 4. Median of Two Sorted Arrays.cpp
@@ -0,0 +1,18 @@
+﻿//4. Median of Two Sorted Arrays
+	//https://leetcode.com/problems/median-of-two-sorted-arrays/description/
+	class Solution
+	{
+		public:
+		double findMedianSortedArrays(vector<int>& nums1 , vector<int>& nums2)
+		{
+			vector<int> nums3;
+			double median = 0;
+			nums3.resize(nums1.size() + nums2.size());
+			merge(nums1.begin() , nums1.end() , nums2.begin() , nums2.end() , nums3.begin());
+
+			if (nums3.size() % 2 == 1)
+				return double(nums3[nums3.size() / 2]);
+			else
+				return double(nums3[nums3.size() / 2 - 1] + nums3[nums3.size() / 2]) / 2;
+		}
+	};
