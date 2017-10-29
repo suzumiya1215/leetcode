83f8bd433e894e58ce6b4984d2c76e2155c2f428
suzumiya1215 <1030011936@qq.com>
2017-10-29 11:51:41 +0800

    121. Best Time to Buy and Sell Stock

---
+++ 121. Best Time to Buy and Sell Stock.cpp
@@ -0,0 +1,26 @@
+﻿class Solution
+ {
+	public:
+
+		int maxProfit(vector<int>& prices) 
+		{
+			int m = prices.size();
+			if (m == 0)
+				return 0;
+			int min = prices[0];
+			int money = 0;
+			for (auto iter : prices)
+			{
+				if (min > iter)
+					min = iter;
+				else
+				{
+					if (money < iter - min)
+						money = iter - min;
+				}
+			
+			}
+			return  money;
+		}
+			
+};
