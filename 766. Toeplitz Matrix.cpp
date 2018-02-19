9c484d918aa38d74459bf0b248bd7798020838ba
suzumiya1215 <suzumiya1215@outlook.com>
2018-02-19 18:31:42 +0800

    766. Toeplitz Matrix

---
+++ 766. Toeplitz Matrix.cpp
@@ -0,0 +1,17 @@
+﻿//https://leetcode.com/problems/toeplitz-matrix/description/
+class Solution
+{
+public:
+	bool isToeplitzMatrix(vector<vector<int> >& matrix)
+	{
+		for (int i = 1; i < matrix[0].size(); ++i)
+		{
+			for (int j = 1; j < matrix.size(); ++j)
+			{
+				if (matrix[j][i] != matrix[j - 1][i - 1])
+					return false;
+			}
+		}
+		return true;
+	}
+};
