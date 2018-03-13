88b6efa8a8b346ee7cbd1b4370d3ffea322a3714
suzumiya1215 <suzumiya1215@outlook.com>
2018-03-13 19:19:05 +0800

    258. Add Digits

---
+++ 258. Add Digits.cpp
@@ -0,0 +1,24 @@
+﻿//https://leetcode.com/problems/add-digits/description/
+class Solution
+{
+public:
+	int addDigits(int num)
+	{
+		string str = to_string(num);
+		while (str.size() != 1)
+		{
+			int n = 0;
+			for (auto iter : str)
+			{
+				int m =iter-48;
+				n += m;
+			}
+			str = to_string(n);
+
+		}
+		int n = atoi(str.c_str());
+		return n;
+	}
+};
+
+//2018年3月13日18:25:20
