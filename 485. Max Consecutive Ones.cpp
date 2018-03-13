3fc6c700587d7c0d773b84f477457f819951baeb
suzumiya1215 <suzumiya1215@outlook.com>
2018-03-13 19:19:19 +0800

    485. Max Consecutive Ones

---
+++ 485. Max Consecutive Ones.cpp
@@ -0,0 +1,19 @@
+﻿//https://leetcode.com/problems/max-consecutive-ones/description/
+class Solution
+{
+public:
+	int findMaxConsecutiveOnes(vector<int>& nums)
+	{
+		int sum = 0;
+		int max = 0;
+		for (auto iter : nums)
+		{
+			sum = (sum + iter)*iter;
+			if (max < sum)
+				max=sum;
+		}
+		return max;
+	}
+};
+
+//2018年3月13日17:49:55
