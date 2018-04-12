d0fe5939b1ba5da5c5e19b59798adb2914c95a56
suzumiya1215 <suzumiya1215@outlook.com>
2018-04-12 23:39:33 +0800

    231. 2的幂

---
+++ 231. 2的幂.cpp
@@ -0,0 +1,15 @@
+﻿//https://leetcode-cn.com/problems/power-of-two/description/
+class Solution
+{
+public:
+	bool isPowerOfTwo(int n)
+	{
+		for (long long i = 0; i != 9999999999; ++i)
+		{
+			if (n == pow(2 , i))
+				return true;
+			if (n < pow(2 , i))
+				return false;
+		}
+	}
+};
